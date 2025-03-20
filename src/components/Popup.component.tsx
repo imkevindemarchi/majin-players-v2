876bf86ed2b79a681604892be9ccd34c41cf2e39
Kevin De Marchi <imkevindemarchi@gmail.com>
2025-03-20 17:46:20 +0100

    bug: Fix the popup component on mobile view

--- src/components/Popup.component.tsx
+++ src/components/Popup.component.tsx
@@ -18,14 +18,14 @@ const Popup: FC = () => {
 
   return (
     <div
-      className={`fixed top-32 px-5 py-2 rounded-lg min-w-60 justify-between items-center mobile:top-20 text-white gap-20 mobile:gap-5 transition-all duration-300 flex
+      className={`fixed top-32 px-5 py-2 rounded-lg min-w-60 justify-between items-center mobile:top-20 text-white gap-20 mobile:gap-5 transition-all duration-300 flex mobile:max-w-[90%]
         ${
           isSuccess ? "bg-green" : isError ? "bg-red" : isWarning && "bg-orange"
         }
         ${isOpen ? "right-5 opacity-100" : "-right-96 opacity-0"}  
       `}
     >
-      <div className="flex gap-2">
+      <div className="flex gap-2 mobile:gap-5 items-center">
         {isSuccess && <CheckIcon className="text-2xl" />}
         {isWarning && <WarningIcon className="text-2xl" />}
         {isError && <ErrorIcon className="text-2xl" />}
