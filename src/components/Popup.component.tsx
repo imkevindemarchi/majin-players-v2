3c366386c71ae843e49b3b359b46176b002cfa0a
Kevin De Marchi <imkevindemarchi@gmail.com>
2025-03-20 17:39:00 +0100

    bug: Fix the popup position

--- src/components/Popup.component.tsx
+++ src/components/Popup.component.tsx
@@ -18,7 +18,7 @@ const Popup: FC = () => {
 
   return (
     <div
-      className={`absolute top-32 px-5 py-2 rounded-lg min-w-60 justify-between items-center mobile:top-20 text-white gap-20 mobile:gap-5 transition-all duration-300 flex
+      className={`fixed top-32 px-5 py-2 rounded-lg min-w-60 justify-between items-center mobile:top-20 text-white gap-20 mobile:gap-5 transition-all duration-300 flex
         ${
           isSuccess ? "bg-green" : isError ? "bg-red" : isWarning && "bg-orange"
         }
